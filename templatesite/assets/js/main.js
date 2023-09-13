682e66a978f44f97169f71c88023c0ee4d8796ee
Tanner Gordon <tannergordon7@gmail.com>
2023-09-12 22:03:17 -0400

    v.something

--- templatesite/assets/js/main.js
+++ templatesite/assets/js/main.js
@@ -155,5 +155,13 @@
 
 			}
 		});
-
-})
+		const counter = document.querySelector(".counter-number");
+		async function updateCounter() {
+			let response = await fetch("https://iyz77t3j5zn6iyraffiyjbyzmy0liexz.lambda-url.us-east-1.on.aws/");
+			let data = await response.json();
+			counter.innerHTML = ` Views: ${data}`;
+		
+		}
+		
+		updateCounter();
+})(jQuery);
